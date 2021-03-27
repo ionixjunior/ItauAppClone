c8a614bccd27c4d5128c8e79e5e7588a418af3bc
Ione Souza Junior <junior@ionixjunior.com.br>
2021-03-27 14:00:24 -0300

    Iniciar a criação do layout da conta corrente

--- ItauAppClone/ContentViews/Home/HomeContentView.cs
+++ ItauAppClone/ContentViews/Home/HomeContentView.cs
@@ -20,7 +20,88 @@ namespace ItauAppClone.ContentViews.Home
                 Children =
                 {
                     new Header(),
-                    new InfoContent("arrow_orange", "Seu limite de crédito continua disponível. Toque aqui.")
+                    new InfoContent("arrow_orange", "Seu limite de crédito continua disponível. Toque aqui."),
+
+                    new Frame
+                    {
+                        HasShadow = false,
+                        Content = new FlexLayout
+                        {
+                            Direction = FlexDirection.Column,
+                            Children =
+                            {
+                                new FlexLayout
+                                {
+                                    JustifyContent = FlexJustify.SpaceBetween,
+                                    Children =
+                                    {
+                                        new Label
+                                        {
+                                            Text = "Saldo em conta corrente"
+                                        }
+                                        .FontSize(Device.GetNamedSize(NamedSize.Title, typeof(Label))),
+
+                                        new FlexLayout
+                                        {
+                                            JustifyContent = FlexJustify.Center,
+                                            AlignItems = FlexAlignItems.Start,
+                                            Children =
+                                            {
+                                                new Label
+                                                {
+                                                    Text = "expandir"
+                                                },
+
+                                                new Image
+                                                {
+                                                    Source = "arrow_orange"
+                                                }
+                                                .Basis(14)
+                                            }
+                                        }
+                                        .Basis(150)
+                                    }
+                                }
+                                .Basis(70),
+
+                                new BoxView
+                                {
+                                    HeightRequest = 1,
+                                    BackgroundColor = Color.FromHex("#EFE9E4")
+                                },
+
+                                new FlexLayout
+                                {
+                                    JustifyContent = FlexJustify.SpaceBetween,
+                                    AlignItems = FlexAlignItems.Start,
+                                    Children =
+                                    {
+                                        new Button
+                                        {
+                                            Text = "ver extrato",
+                                            TextColor = Color.FromHex("#0D6EB0"),
+                                            BackgroundColor = Color.Transparent,
+                                            TextTransform = TextTransform.Lowercase,
+                                            FontAttributes = FontAttributes.Bold
+                                        }
+                                        .FontSize(Device.GetNamedSize(NamedSize.Medium, typeof(Button))),
+
+                                        new Button
+                                        {
+                                            Text = "ver calendário",
+                                            TextColor = Color.FromHex("#0D6EB0"),
+                                            BackgroundColor = Color.Transparent,
+                                            TextTransform = TextTransform.Lowercase,
+                                            FontAttributes = FontAttributes.Bold
+                                        }
+                                        .FontSize(Device.GetNamedSize(NamedSize.Medium, typeof(Button)))
+                                    }
+                                }
+                                .Basis(40)
+                            }
+                        }
+                    }
+                    .Margin(20)
                 }
             };
         }
