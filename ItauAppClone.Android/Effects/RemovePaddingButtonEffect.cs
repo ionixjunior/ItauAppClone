871f9612b3beb1faf0a815af9fc3769d6d7a5023
Ione Souza Junior <junior@ionixjunior.com.br>
2021-04-24 12:46:44 -0300

    Tentando criar effect para remover padding do botão do Android

---
+++ ItauAppClone.Android/Effects/RemovePaddingButtonEffect.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ItauAppClone.Droid.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ResolutionGroupName("AppClone")]
+[assembly: ExportEffect(typeof(RemovePaddingButtonEffect), nameof(RemovePaddingButtonEffect))]
+namespace ItauAppClone.Droid.Effects
+{
+    public class RemovePaddingButtonEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            try
+            {
+                if (Control is Android.Widget.Button button)
+                {
+                    button.SetMinWidth(0);
+                    button.SetPadding(0, button.PaddingTop, 0, button.PaddingBottom);
+                }
+            }
+            catch (Exception exception)
+            {
+                System.Diagnostics.Debug.WriteLine(exception.Message);
+            }
+        }
+
+        protected override void OnDetached()
+        {
+            
+        }
+    }
+}
---
+++ ItauAppClone/Effects/RemovePaddingButtonEffect.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace ItauAppClone.Effects
+{
+    public class RemovePaddingButtonEffect : RoutingEffect
+    {
+        public RemovePaddingButtonEffect() : base($"AppClone.{nameof(RemovePaddingButtonEffect)}")
+        {
+        }
+    }
+}
