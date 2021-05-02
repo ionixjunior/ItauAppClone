cc2b872ab740324d5696ed53cbe43ec8b43f3916
Ione Souza Junior <junior@ionixjunior.com.br>
2021-05-02 10:32:23 -0300

    Substituir Grid do CardInfoContent pelo controle InfoContent

--- ItauAppClone/Controls/CardInfoContent.cs
+++ ItauAppClone/Controls/CardInfoContent.cs
@@ -1,48 +1,16 @@
-﻿using System;
-using Xamarin.CommunityToolkit.Markup;
-using Xamarin.Forms;
-using static Xamarin.CommunityToolkit.Markup.GridRowsColumns;
+﻿using Xamarin.Forms;
 
 namespace ItauAppClone.Controls
 {
     public class CardInfoContent : Frame
     {
-        enum GridColumn { Left, Right }
-
         public CardInfoContent(string icon, string text)
         {
             BackgroundColor = Color.FromHex("#0D6EB0");
             HasShadow = false;
             CornerRadius = 5;
             Margin = new Thickness(20);
-
-            Content = new Grid
-            {
-                ColumnDefinitions = Columns.Define(
-                    (GridColumn.Left, 40),
-                    (GridColumn.Right, Star)
-                ),
-
-                ColumnSpacing = 20,
-
-                Children =
-                {
-                    new Image
-                    {
-                        Source = icon
-                    }
-                    .Column(GridColumn.Left),
-
-                    new Label
-                    {
-                        Text = text,
-                        TextColor = Color.White,
-                        FontAttributes = FontAttributes.Bold
-                    }
-                    .Column(GridColumn.Right)
-                    .FontSize(Device.GetNamedSize(NamedSize.Large, typeof(Label)))
-                }
-            };
+            Content = new InfoContent(icon, text);
         }
     }
 }
