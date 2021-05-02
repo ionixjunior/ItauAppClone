c17d187b5513919a607f0cbfe22fd565ceb9ddd3
Ione Souza Junior <junior@ionixjunior.com.br>
2021-05-02 10:28:44 -0300

    Criar controle InfoContent

---
+++ ItauAppClone/Controls/InfoContent.cs
@@ -0,0 +1,39 @@
+﻿using Xamarin.CommunityToolkit.Markup;
+using Xamarin.Forms;
+using static Xamarin.CommunityToolkit.Markup.GridRowsColumns;
+
+namespace ItauAppClone.Controls
+{
+    public class InfoContent : Grid
+    {
+        enum GridColumn { Left, Right }
+
+        public InfoContent(string icon, string text)
+        {
+            ColumnDefinitions = Columns.Define(
+                (GridColumn.Left, 40),
+                (GridColumn.Right, Star)
+            );
+            ColumnSpacing = 20;
+
+            Children.Add(
+                new Image
+                {
+                    Source = icon
+                }
+                .Column(GridColumn.Left)
+            );
+
+            Children.Add(
+                new Label
+                {
+                    Text = text,
+                    TextColor = Color.White,
+                    FontAttributes = FontAttributes.Bold
+                }
+                .Column(GridColumn.Right)
+                .FontSize(Device.GetNamedSize(NamedSize.Large, typeof(Label)))
+            );
+        }
+    }
+}
