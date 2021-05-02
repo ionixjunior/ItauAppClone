f450b9aa7ccca6a4ebd5d60e9868e0e8f536e5b1
Ione Souza Junior <junior@ionixjunior.com.br>
2021-05-02 12:35:17 -0300

    Implementar OnDetach no effect

--- ItauAppClone.Android/Effects/RemovePaddingButtonEffect.cs
+++ ItauAppClone.Android/Effects/RemovePaddingButtonEffect.cs
@@ -9,12 +9,20 @@ namespace ItauAppClone.Droid.Effects
 {
     public class RemovePaddingButtonEffect : PlatformEffect
     {
+        private int _minWidth;
+        private int _paddingLeft;
+        private int _paddingRight;
+
         protected override void OnAttached()
         {
             try
             {
                 if (Control is Android.Widget.Button button)
                 {
+                    _minWidth = button.MinWidth;
+                    _paddingLeft = button.PaddingLeft;
+                    _paddingRight = button.PaddingRight;
+
                     button.SetMinWidth(0);
                     button.SetPadding(0, button.PaddingTop, 0, button.PaddingBottom);
                 }
@@ -27,7 +35,18 @@ namespace ItauAppClone.Droid.Effects
 
         protected override void OnDetached()
         {
-            
+            try
+            {
+                if (Control is Android.Widget.Button button)
+                {
+                    button.SetMinWidth(_minWidth);
+                    button.SetPadding(_paddingLeft, button.PaddingTop, _paddingRight, button.PaddingBottom);
+                }
+            }
+            catch (Exception exception)
+            {
+                System.Diagnostics.Debug.WriteLine(exception.Message);
+            }
         }
     }
 }
