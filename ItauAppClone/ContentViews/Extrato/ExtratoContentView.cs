895a2b1dfe4ce2623a841f32f4c2f663d15b875e
Ione Souza Junior <junior@ionixjunior.com.br>
2021-05-08 12:12:46 -0300

    Adicionar float action button na tela do extrato

--- ItauAppClone/ContentViews/Extrato/ExtratoContentView.cs
+++ ItauAppClone/ContentViews/Extrato/ExtratoContentView.cs
@@ -9,23 +9,48 @@ namespace ItauAppClone.ContentViews.Extrato
 {
     public class ExtratoContentView : ContentView, IReload
     {
-        enum GridRow { Header, Content }
+        enum LinhaGrid { Cabecalho, Conteudo }
 
         public ExtratoContentView() => Build();
 
         public void Build()
         {
+            Margin = new Thickness(0, 0, 0, 22);
+
             Content = new Grid
             {
                 RowDefinitions = Rows.Define(
-                    (GridRow.Header, 60),
-                    (GridRow.Content, Star)
+                    (LinhaGrid.Cabecalho, 60),
+                    (LinhaGrid.Conteudo, Star)
                 ),
                 RowSpacing = 0,
 
                 Children =
                 {
-                    new Filtros()
+                    new Filtros().Row(LinhaGrid.Cabecalho),
+                    new Grid
+                    {
+                        Children =
+                        {
+                            new CollectionView
+                            {
+                            },
+
+                            new Frame
+                            {
+                                BackgroundColor = Color.FromHex("#007AB7"),
+                                HasShadow = false,
+                                CornerRadius = 30
+                            }
+                            .End()
+                            .Bottom()
+                            .Margin(20)
+                            .Width(60)
+                            .Height(60)
+                            .Padding(0)
+                        }
+                    }
+                    .Row(LinhaGrid.Conteudo)
                 }
             };
         }
