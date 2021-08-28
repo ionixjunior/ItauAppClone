89a6f0a03bd5e6bac3dd71095976e33cfe728613
Ione Souza Junior <junior@ionixjunior.com.br>
2021-08-28 10:30:09 -0300

    Extrair para um método o carregamento da lista de transações

--- ItauAppClone/ViewModels/ExtratoContentViewModel.cs
+++ ItauAppClone/ViewModels/ExtratoContentViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using ItauAppClone.Enums;
 using ItauAppClone.Models;
@@ -11,11 +12,18 @@ namespace ItauAppClone.ViewModels
 
         public ExtratoContentViewModel()
         {
-            Transacoes = new ObservableCollection<Transacao>();
-            Transacoes.Add(new Transacao("ted 123456", 1512.07m, TipoTransacao.Entrada, new DateTime(2021, 2, 26)));
-            Transacoes.Add(new Transacao("int ted d 957609", 977.00m, TipoTransacao.Saida, new DateTime(2021, 2, 25)));
-            Transacoes.Add(new Transacao("ted 102.0003marcos w c s", 1000.00m, TipoTransacao.Entrada, new DateTime(2021, 2, 25)));
-            Transacoes.Add(new Transacao("int ted d 545387", 500.00m, TipoTransacao.Saida, new DateTime(2021, 2, 23)));
+            Transacoes = new ObservableCollection<Transacao>(CarregarTransacoes());
+        }
+
+        private IList<Transacao> CarregarTransacoes()
+        {
+            return new List<Transacao>
+            {
+                new Transacao("ted 123456", 1512.07m, TipoTransacao.Entrada, new DateTime(2021, 2, 26)),
+                new Transacao("int ted d 957609", 977.00m, TipoTransacao.Saida, new DateTime(2021, 2, 25)),
+                new Transacao("ted 102.0003marcos w c s", 1000.00m, TipoTransacao.Entrada, new DateTime(2021, 2, 25)),
+                new Transacao("int ted d 545387", 500.00m, TipoTransacao.Saida, new DateTime(2021, 2, 23))
+            };
         }
     }
 }
