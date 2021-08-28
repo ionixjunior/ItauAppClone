07a2e99c6179ee8f5b67cc578efac80b03463424
Ione Souza Junior <junior@ionixjunior.com.br>
2021-08-28 11:44:35 -0300

    Adicionar mais transações na lista

--- ItauAppClone/ViewModels/ExtratoContentViewModel.cs
+++ ItauAppClone/ViewModels/ExtratoContentViewModel.cs
@@ -25,7 +25,11 @@ namespace ItauAppClone.ViewModels
                 new Transacao("ted 123456", 1512.07m, TipoTransacao.Entrada, new DateTime(2021, 2, 26)),
                 new Transacao("int ted d 957609", 977.00m, TipoTransacao.Saida, new DateTime(2021, 2, 25)),
                 new Transacao("ted 102.0003marcos w c s", 1000.00m, TipoTransacao.Entrada, new DateTime(2021, 2, 25)),
-                new Transacao("int ted d 545387", 500.00m, TipoTransacao.Saida, new DateTime(2021, 2, 23))
+                new Transacao("int ted d 545387", 500.00m, TipoTransacao.Saida, new DateTime(2021, 2, 23)),
+                new Transacao("ted 123456", 1512.07m, TipoTransacao.Entrada, new DateTime(2021, 2, 23)),
+                new Transacao("int ted d 957609", 977.00m, TipoTransacao.Saida, new DateTime(2021, 2, 23)),
+                new Transacao("ted 102.0003marcos w c s", 1000.00m, TipoTransacao.Entrada, new DateTime(2021, 2, 23)),
+                new Transacao("int ted d 545387", 500.00m, TipoTransacao.Saida, new DateTime(2021, 2, 22))
             };
         }
     }
