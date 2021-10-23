dd6beff8f1b196b92b705232828ff9c47a7ba978
Ione Souza Junior <junior@ionixjunior.com.br>
2021-10-23 10:29:37 -0300

    Implementar IReload para o hot reload no C# funcionar

--- ItauAppClone/ContentViews/Transacao/TransacaoContentView.cs
+++ ItauAppClone/ContentViews/Transacao/TransacaoContentView.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-
+using ItauAppClone.Interfaces;
 using Xamarin.Forms;
 
 namespace ItauAppClone.ContentViews.Transacao
 {
-    public class TransacaoContentView : ContentView
+    public class TransacaoContentView : ContentView, IReload
     {
-        public TransacaoContentView()
+        public TransacaoContentView() => Build();
+
+        public void Build()
         {
             Content = new Label { Text = "Hello ContentView" };
         }
