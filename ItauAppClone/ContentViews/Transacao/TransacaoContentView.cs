10c261cd2caa850cbb1e25cf3c079568a7d5ffd1
Ione Souza Junior <junior@ionixjunior.com.br>
2021-10-23 11:16:57 -0300

    Criar botões da página

--- ItauAppClone/ContentViews/Transacao/TransacaoContentView.cs
+++ ItauAppClone/ContentViews/Transacao/TransacaoContentView.cs
@@ -26,7 +26,66 @@ namespace ItauAppClone.ContentViews.Transacao
 
             grid.Children.Add(new Header().Row(GridRow.Cabecalho));
 
+            grid.Children.Add(new ScrollView
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label { Text = "hoje, dia 05,\no que deseja fazer?" },
+
+                        new FlexLayout
+                        {
+                            Direction = FlexDirection.Row,
+                            Wrap = FlexWrap.Wrap,
+
+                            Children =
+                            {
+                                ObterBotaoPequeno(),
+                                ObterBotaoPequeno(),
+                                ObterBotaoPequeno(),
+                                ObterBotaoPequeno(),
+                                ObterBotaoGrande(),
+                                ObterBotaoPequeno(),
+                                ObterBotaoPequeno(),
+                                ObterBotaoPequeno(),
+                                ObterBotaoPequeno(),
+                            }
+                        }
+                    }
+                }
+            }
+            .Row(GridRow.Conteudo));
+
             Content = grid;
+        }
+
+        private View ObterBotaoPequeno()
+        {
+            return new Frame
+            {
+                HasShadow = false,
+                CornerRadius = 4,
+                HeightRequest = 100,
+                BackgroundColor = Color.Red
+            }
+            .Padding(0)
+            .Margin(8)
+            .Basis(new FlexBasis(0.5f, true));
+        }
+
+        private View ObterBotaoGrande()
+        {
+            return new Frame
+            {
+                HasShadow = false,
+                CornerRadius = 4,
+                HeightRequest = 160,
+                BackgroundColor = Color.Blue
+            }
+            .Padding(0)
+            .Margin(8)
+            .Basis(new FlexBasis(1f, true));
         }
     }
 }
