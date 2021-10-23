c246fa6eb43c45aecbec1b5549bbc55381b14c3d
Ione Souza Junior <junior@ionixjunior.com.br>
2021-10-23 10:53:11 -0300

    Adicionar cabeçalho da tela de transações

--- ItauAppClone/ContentViews/Transacao/TransacaoContentView.cs
+++ ItauAppClone/ContentViews/Transacao/TransacaoContentView.cs
@@ -1,16 +1,32 @@
 ﻿using System;
+using ItauAppClone.Controls;
 using ItauAppClone.Interfaces;
+using Xamarin.CommunityToolkit.Markup;
 using Xamarin.Forms;
+using static Xamarin.CommunityToolkit.Markup.GridRowsColumns;
 
 namespace ItauAppClone.ContentViews.Transacao
 {
     public class TransacaoContentView : ContentView, IReload
     {
+        enum GridRow { Cabecalho, Conteudo }
+
         public TransacaoContentView() => Build();
 
         public void Build()
         {
-            Content = new Label { Text = "Hello ContentView" };
+            var grid = new Grid
+            {
+                RowDefinitions = Rows.Define(
+                    (GridRow.Cabecalho, 60),
+                    (GridRow.Conteudo, Star)
+                ),
+                RowSpacing = 0
+            };
+
+            grid.Children.Add(new Header().Row(GridRow.Cabecalho));
+
+            Content = grid;
         }
     }
 }
